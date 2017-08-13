e7bfb3d4b3fd8bd72dc40cef2c3d6ae3c69e00d5
Geoffrey <geo.galagaran@gmail.com>
2017-08-14 00:28:46 +0800

    Move print_data function as a Locker method

--- cmd_prints.py
+++ cmd_prints.py
@@ -17,36 +17,3 @@ def ask_option_number(text="Enter Option Number: "):
         return int(input(text))
     except:
         return input(text)
-
-
-def print_data(data):
-    """
-        Prints the list of stored password, gives the option to select from said list
-        and prints the password details for valid data selections
-    """
-    if data:
-        data_keys = list(data.keys())
-
-        print("""
-        Enter a number from the list below to show details:
-        Press 'c' to cancel
-        """)
-        # Print list of stored account passwords
-        print('\n'.join('\t%i: %s' % (index, app_name) for index, app_name in enumerate(data_keys)))
-        selected_option = input('\nSelect Password Number: ').lower()
-
-        # Try-Except whether the given selected_option is a valid index on data_keys
-        try:
-            index = int(selected_option)
-            pw_account = data_keys[index]
-            pw_details = data[pw_account]
-            print("\n\tPASSWORD_FOR: %s" % pw_account)
-            [ print("\t%s: %s" % (key, value)) for key, value in pw_details.items() ]
-            print("\n")
-        except:
-            if selected_option.isalpha() and selected_option == 'c':
-                return
-            else:
-                print("\n\tInvalid option number\n")
-    else:
-        print("\n\tNo passwords currently stored.\n")
--- manager.py
+++ manager.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     from locker import Locker
-    from cmd_prints import print_options, ask_option_number, print_data
+    from cmd_prints import print_options, ask_option_number
 
     pw_manager = Locker()
     data = pw_manager.get_locker_data()
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     while selected_option != 0:
         if selected_option == 1:
-            print_data(data)
+            pw_manager.print_data()
         elif selected_option == 5:
             print_options()
 
