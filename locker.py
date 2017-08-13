1d4cc61abc832e42f0b97fab07a21647628fadf0
Geoffrey <geo.galagaran@gmail.com>
2017-08-14 03:24:01 +0800

    Add "add_password" method; Move "print_options" from cmd.py as a Locker class method

--- locker.py
+++ locker.py
@@ -1,4 +1,5 @@
 import os
+import bcrypt
 
 class Locker:
     def __init__(self, filename='locker'):
@@ -40,7 +41,6 @@ class Locker:
                 filename:
                     The location/filename where the data is to be stored
         """
-
         data = ''
         data_content = locker_data.items()
 
@@ -55,23 +55,43 @@ class Locker:
             locker_file.write(data)
 
 
+    @staticmethod
+    def print_options():
+        """ Prints the Password Manager Options """
+        print("""
+        PASSWORD MANAGER
+        by Geoffrey
+
+        Options:
+        1: Show Passwords         4: Delete Password
+        2: Add Password           5: Show Options
+        3: Change Password        0: Exit Password Manager
+        """)
+
+
+    @staticmethod
+    def ask_option_number(text="[Enter Option Number]: "):
+        """ Asks for an input printing based on the given parameter text """
+        try:
+            return int(input(text))
+        except:
+            return input(text)
+
+
     def print_data(self):
         """
             Prints the list of stored password, gives the option to select from said list
             and prints the password details for valid data selections
         """
         data = self.__file_data
-        
+
         if data:
             data_keys = list(data.keys())
 
-            print("""
-            Enter a number from the list below to show details:
-            Press 'c' to cancel
-            """)
+            print("\n\tEnter a number from the list below to show details:\n\tPress 'c' to cancel\n")
             # Print list of stored account passwords
             print('\n'.join('\t%i: %s' % (index, app_name) for index, app_name in enumerate(data_keys)))
-            selected_option = input('\nSelect Password Number: ').lower()
+            selected_option = input('\n[Select password details for]: ').lower()
 
             # Try-Except whether the given selected_option is a valid index on data_keys
             try:
@@ -88,3 +108,89 @@ class Locker:
                     print("\n\tInvalid option number\n")
         else:
             print("\n\tNo passwords currently stored.\n")
+
+
+    def _add_password_data(self, should_encrypt='y'):
+        """ Auxiliary Function for add_password that does the true adding work """
+        data = self.__file_data
+        app_name = input("\n\t[Where is this password used for?]: ").title()
+
+        while len(app_name) < 3:
+            print("\n\tUSAGE NAME MUST NOT BE LESS THAN 3 CHARACTERS")
+            app_name = input("\n\t[Where is this password used for?]: ").title()
+
+        # Print an error that the application name has already been stored
+        if app_name in data:
+            print("\n\tAbort: You already have a password stored for '%s'" % app_name)
+            return
+
+        data[app_name] = {}
+        app = data[app_name]
+
+        def encryptor(should_encrypt):
+            def wrapper(func):
+                def inner(*args, **kwargs):
+                    secret = func(*args, **kwargs)
+
+                    while len(secret) < 3:
+                        print("\n\tPASSWORD MUST NOT BE LESS THAN 3 CHARACTERS")
+                        secret = func(*args, **kwargs)
+
+                    if should_encrypt == 'y':
+                        hint = input("\n\t[Enter Password Hint]: ")
+
+                        while len(hint) < 3:
+                            print("\n\tHINT MUST NOT BE LESS THAN 3 CHARACTERS")
+                            hint = input("\n\t[Enter Password Hint]: ")
+
+                        # Bcrypt hash encrypt the secret password
+                        secret = bcrypt.hashpw(str.encode(secret), bcrypt.gensalt()).decode()
+                        app["HINT"] = hint
+
+                    app["SECRET"] = secret
+                    print("\n\tPASSWORD_FOR: %s" % app_name)
+                    [ print("\t%s: %s" % (key, value)) for key, value in app.items() ]
+                    should_store = input("\n\t[Store this password?[Y/N/C]]: ").lower()
+
+                    while should_store not in 'ync':
+                        should_store = input("[Store this password?[Y/N/C]]: ").lower()
+                    else:
+                        # Delete created password detail if canceled or should not be stored
+                        if should_store in 'nc':
+                            del data[app_name]
+                    return should_store
+                return inner
+            return wrapper
+
+        @encryptor(should_encrypt)
+        def pw_details():
+            return input("\n\t[Enter desired password]: ")
+
+        should_store = pw_details()
+
+        while should_store == 'n':
+            return self._add_password_data(should_encrypt)
+        else:
+            if should_store == 'y':
+                self.save(data)
+                print("===SAVE SUCCESS===")
+                return
+            elif should_store == 'c':
+                print('canceled')
+                return
+
+
+    def add_password(self):
+        """ Adds a new password data to the locker """
+        print("\n\tDo you want the password to be encrypted? [Y/N]\n\tPress 'c' to cancel")
+        selected_option = input("\n[Encrypt Password?]: ").lower()
+
+        if selected_option.isalpha():
+            if selected_option == 'c':
+                return
+            elif selected_option not in 'ync':
+                print("\n\tInvalid Answer: Must be [Y/N/C]\n")
+            else:
+                self._add_password_data(selected_option)
+        else:
+            print("\n\tInvalid Answer: Must be [Y/N/C]\n")
--- manager.py
+++ manager.py
@@ -1,18 +1,18 @@
 if __name__ == '__main__':
     from locker import Locker
-    from cmd import print_options, ask_option_number
 
     pw_manager = Locker()
     data = pw_manager.get_locker_data()
 
-    print_options()
-    selected_option = ask_option_number()
+    pw_manager.print_options()
+    selected_option = pw_manager.ask_option_number()
 
     while selected_option != 0:
         if selected_option == 1:
             pw_manager.print_data()
+        elif selected_option == 2:
+            pw_manager.add_password()
         elif selected_option == 5:
-            print_options()
+            pw_manager.print_options()
 
-
-        selected_option = ask_option_number()
+        selected_option = pw_manager.ask_option_number()
